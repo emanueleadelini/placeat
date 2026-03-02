1382083b56c17bbae28f7225cec7ea3a147f660a
Emanuele Adelini <EmanueleAdelini@gmail.com>
2026-03-02 11:55:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5252657656-b7ce3",
+  "appId": "1:548783751049:web:fb753e8b0517e065b42b58",
+  "apiKey": "AIzaSyAjIYQMSjxEtCpbRbeD54z8yxcKaKgPNfE",
+  "authDomain": "studio-5252657656-b7ce3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "548783751049"
+};
